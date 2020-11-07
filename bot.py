d43baa039a24c4af58fc3ce9d016d1bb9bd2a2d9
Johnfishmaster <51709412+Johnfishmaster@users.noreply.github.com>
2020-11-07 16:54:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,11 @@ import discord
 import files
 
 bot = discord.Client()
-token = files.read("token.pkl")
+try:
+    token = files.read("data/token.pkl")
+except FileNotFoundError:
+    print("Your token file is missing. Put it in the data folder")
+    exit(1)
 
 # TODO
 # Delete leaderboard messages
@@ -19,19 +23,19 @@ async def on_ready():
 async def on_message(message):
     # Ignore own messages unless they are leaderboard messages, if so save them to the guild file
     if message.author == bot.user and (message.content.startswith == "---------Leaderboard----------" or message.content.startswith('Type "Update" to get a new leaderboard') or message.content.startswith("There are no Counters on")):
-        data = files.read(f"{message.guild.id}.pkl")
+        data = files.read(f"data/{message.guild.id}.pkl")
         data["leaderboard_message_id"] = message.id
         data["leaderboard_message_channel_id"] = message.channel.id
-        files.write(f"{message.guild.id}.pkl", data)
+        files.write(f"data/{message.guild.id}.pkl", data)
     if message.author.bot:
         return
 
     # Put server's data into var data for efficiency
     try:
-        data = files.read(f"{message.guild.id}.pkl")
+        data = files.read(f"data/{message.guild.id}.pkl")
     except FileNotFoundError:
-        files.write(f"{message.guild.id}.pkl", {"count": 1})
-        data = files.read(f"{message.guild.id}.pkl")
+        files.write(f"data/{message.guild.id}.pkl", {"count": 1})
+        data = files.read(f"data/{message.guild.id}.pkl")
     except AttributeError:
         await message.channel.send("Sorry you have to be on a server to count")
         return
@@ -108,7 +112,7 @@ async def on_message(message):
             data[message.author.id] = 1
 
         # updating the files
-        files.write(f"{message.guild.id}.pkl", data)
+        files.write(f"data/{message.guild.id}.pkl", data)
 
     else:
 
