ed43b2b2e05653fc444e9b0f2c81be4b43241556
Johnfishmaster <51709412+Johnfishmaster@users.noreply.github.com>
2020-11-09 15:23:14 +0100

    Update messages are now being deleted

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
 # CountingBot
 A discord bot to let users count in the #count channel
 
-I will probably automate this process in the next update
 
-Setup :
-
-1:  Start a python shell in the bot directory and use the following commands
-
-  * import files
-  * token = [YOUR TOKEN]
-  * files.write("data/token.pkl", token)
-
-Done
+Everything is automatic
--- bot.py
+++ bot.py
@@ -43,83 +43,82 @@ async def on_message(message):
     expected_number = data["count"]
     print(f"expected number = {expected_number}")
     # dont count if messages are not send in the counting channel
-    if str(message.channel) != "counting":
-
-        if str(message.channel) == "leaderboard":
-
-            if message.content.lower() == "update":
-                try:
-                    # Retrieve the message and channel id and delete the old message
-                    try:
-                        channel = await bot.fetch_channel(data["leaderboard_message_channel_id"])
-                        msg = await channel.fetch_message(data["leaderboard_message_id"])
-                        await msg.delete()
-                    except discord.NotFound:
-                        pass
-                except KeyError:
-                    pass
-                # List all Counters
-                counters_value = {}
-                counters = []
-                for user in data:
-                    if type(user) == int:
-                        counters.append([data[user], str(await bot.fetch_user(user))])
-                if not counters:
-                    await message.channel.send(f"There are no Counters on {message.guild} :(")
-                else:
-                    # Sort output
-                    counters.sort()
-                    counters.reverse()
-                    board = "---------Leaderboard-----------------------------\n"
-                    for place in range(len(counters)):
-                        board += f"{place + 1}:{counters[place][1]}:{counters[place][0]}\n"
-
-                    await message.channel.send(board)
-
-            else:
-                await message.channel.send('Type "Update" to get a new leaderboard ')
-            await message.delete()
-        return
-
-    # Check if message is a number
-    try:
-        message_number = int(message.content)
-    except ValueError:
-        # If not delete the message
-        await message.delete()
-        return
-
-    # ignore if the author of the message was the last one counting
-    try:
-        if message.author.id == data["last_counter"]:
-            await message.channel.send(f"Sie sind der letzte der Zählte, **{message.author}**!")
+    if str(message.channel) == "counting":
+        # Check if message is a number
+        try:
+            message_number = int(message.content)
+        except ValueError:
+            # If not delete the message
             await message.delete()
             return
-    except KeyError:
-        pass
 
-    # delete message if the number isn't the one we're searching for
-    if message_number == expected_number:
-        # Update Values
-
-        data["count"] = data["count"] + 1
-        data["last_counter"] = message.author.id
+        # ignore if the author of the message was the last one counting
         try:
-            # Read participation data add 1 to user's value
-            participation_user = data[message.author.id]
-            participation_user = participation_user + 1
-            data[message.author.id] = participation_user
-
+            if message.author.id == data["last_counter"]:
+                await message.channel.send(f"Sie sind der letzte der Zählte, **{message.author}**!")
+                await message.delete()
+                return
         except KeyError:
-            # If participation is not there yet set it to one
-            data[message.author.id] = 1
+            pass
 
-        # updating the files
-        files.write(f"data/{message.guild.id}.pkl", data)
+        # delete message if the number isn't the one we're searching for
+        if message_number == expected_number:
+            # Update Values
 
-    else:
+            data["count"] = data["count"] + 1
+            data["last_counter"] = message.author.id
+            try:
+                # Read participation data add 1 to user's value
+                participation_user = data[message.author.id]
+                participation_user = participation_user + 1
+                data[message.author.id] = participation_user
 
+            except KeyError:
+                # If participation is not there yet set it to one
+                data[message.author.id] = 1
+
+            # updating the files
+            files.write(f"data/{message.guild.id}.pkl", data)
+
+        else:
+
+            await message.delete()
+
+    if str(message.channel) == "leaderboard":
+        try:
+            # Retrieve the message and channel id and delete the old message
+            try:
+                channel = await bot.fetch_channel(data["leaderboard_message_channel_id"])
+                msg = await channel.fetch_message(data["leaderboard_message_id"])
+                await msg.delete()
+            except discord.NotFound:
+                pass
+        except KeyError:
+            pass
+        if message.content.lower() == "update":
+
+            # List all Counters
+            counters_value = {}
+            counters = []
+            for user in data:
+                if type(user) == int:
+                    counters.append([data[user], str(await bot.fetch_user(user))])
+            if not counters:
+                await message.channel.send(f"There are no Counters on {message.guild} :(")
+            else:
+                # Sort output
+                counters.sort()
+                counters.reverse()
+                board = "---------Leaderboard-----------------------------\n"
+                for place in range(len(counters)):
+                    board += f"{place + 1}:{counters[place][1]}:{counters[place][0]}\n"
+
+                await message.channel.send(board)
+
+        else:
+            await message.channel.send('Type "Update" to get a new leaderboard ')
         await message.delete()
+    return
 
 
 # Run the bot
